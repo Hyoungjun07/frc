927aab92940ed342ea3ffccf3fccadc788b824a3
Jeong Jeon <Luckyjeon07@gmail.com>
2026-01-10 16:38:45 -0800

    second push

--- client/src/routes/dtf.jsx
+++ client/src/routes/dtf.jsx
@@ -2,6 +2,39 @@ import React from 'react'
 
 export default function Dtf() {
   return (
-    <div>dtf</div>
+    <div>
+      <h1> Drive Team Feeder </h1>
+        <h2> Alliance 1 </h2>
+        <form>
+          <label> Team 1 Number</label>
+            <div>
+              <input type='number'/>
+            </div>
+          <label> Team 2 Number</label>
+          <div>
+              <input type='number'/>
+            </div>
+          <label> Team 3 Number</label>
+          <div>
+              <input type='number'/>
+            </div>
+        <h2> Alliance 2 </h2>
+          <label> Team 1 Number</label>
+          <div>
+              <input type='number'/>
+            </div>
+          <label> Team 2 Number</label>
+          <div>
+              <input type='number'/>
+            </div>
+          <label> Team 3 Number</label>
+          <div>
+              <input type='number'/>
+            </div>
+            <h3>
+              <button>submit</button>
+            </h3>
+      </form> 
+    </div>
   )
 }
